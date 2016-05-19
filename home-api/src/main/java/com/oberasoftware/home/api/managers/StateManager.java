a21dc79e8a8981236fc31e9e2cfea1e33aaee298
Renze de Vries <renze@renarj.nl>
2016-05-19 21:10:48 +0200

    Added value transport and refactored statemanager

--- home-api/src/main/java/com/oberasoftware/home/api/managers/StateManager.java
+++ home-api/src/main/java/com/oberasoftware/home/api/managers/StateManager.java
@@ -1,8 +1,6 @@
 package com.oberasoftware.home.api.managers;
 
 import com.oberasoftware.home.api.model.State;
-import com.oberasoftware.home.api.model.Status;
-import com.oberasoftware.home.api.model.storage.DeviceItem;
 import com.oberasoftware.home.api.types.Value;
 
 import java.util.Map;
@@ -11,13 +9,9 @@ import java.util.Map;
  * @author renarj
  */
 public interface StateManager {
-    State updateItemState(String itemId, String label, Value value);
+    State updateItemState(String controllerId, String itemId, String label, Value value);
 
-    State updateDeviceState(DeviceItem item, String label, Value value);
+    State getItemState(String controllerId, String itemId);
 
-    State updateStatus(DeviceItem item, Status newStatus);
-
-    Map<String, State> getStates();
-
-    State getState(String itemId);
+    Map<String, State> getStates(String controllerId);
 }
